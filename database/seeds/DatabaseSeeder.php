74ff2be1a8e6aa2134860a615d4d47f9a7f6cf67
Abigail Palacios <abigailpalacios6@gmail.com>
2021-04-04 15:44:40 -0600

    Update seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,25 +10,25 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $account = Account::create(['name' => 'Acme Corporation']);
+        $account = Account::create(['name' => 'CREATIVA']);
 
         factory(User::class)->create([
             'account_id' => $account->id,
-            'first_name' => 'John',
-            'last_name' => 'Doe',
-            'email' => 'johndoe@example.com',
+            'first_name' => 'Katherine',
+            'last_name' => 'Quintanilla',
+            'email' => 'creativa19@gmail.com',
             'owner' => true,
         ]);
 
-        factory(User::class, 5)->create(['account_id' => $account->id]);
-
-        $organizations = factory(Organization::class, 100)
-            ->create(['account_id' => $account->id]);
-
-        factory(Contact::class, 100)
-            ->create(['account_id' => $account->id])
-            ->each(function ($contact) use ($organizations) {
-                $contact->update(['organization_id' => $organizations->random()->id]);
-            });
+//        factory(User::class, 5)->create(['account_id' => $account->id]);
+//
+//        $organizations = factory(Organization::class, 100)
+//            ->create(['account_id' => $account->id]);
+//
+//        factory(Contact::class, 100)
+//            ->create(['account_id' => $account->id])
+//            ->each(function ($contact) use ($organizations) {
+//                $contact->update(['organization_id' => $organizations->random()->id]);
+//            });
     }
 }
