8234881469163c44347ec9e138cf6d46a49c3b9d
Abigail Palacios <abigailpalacios6@gmail.com>
2021-04-06 16:58:25 -0600

    Create products migration

---
+++ database/migrations/2021_04_06_225236_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('contact_id')->nullable()->index();
+            $table->string('name', 25);
+            $table->integer('quantity');
+            $table->string('description', 50)->nullable();
+            $table->float('unit_cost');
+            $table->float('total_cost');
+            $table->string('notes', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
